4d257cdc1ddb7ed1860d1a20b42897a1c530405e
davidfitzek <david.fitzek@protonmail.com>
2023-04-17 11:17:18 -0400

    add KLLoss

--- src/rydberggpt/training/loss.py
+++ src/rydberggpt/training/loss.py
@@ -3,6 +3,19 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+class KLLoss(nn.Module):
+    def __init__(self):
+        super(KLLoss, self).__init__()
+
+    def forward(self, cond_log_probs, tgt):
+        batchsize = tgt.shape[0]
+        # cond_probs = torch.exp(cond_log_probs)  # used for debugging
+        temp = torch.einsum("bnd,bnd->bn", cond_log_probs, tgt)
+        log_probs = torch.sum(temp, axis=-1)
+        loss = -torch.sum(log_probs) / batchsize
+        return loss
+
+
 class LabelSmoothing(nn.Module):
     """Implement label smoothing for a classification task. Label smoothing is a regularization
     technique that smooths the probability distribution of the target labels by replacing the
@@ -24,14 +37,20 @@ class LabelSmoothing(nn.Module):
         Args:
             cond_probs (torch.Tensor): Tensor of shape (batch_size, num_atoms, dim) containing the
                 predicted conditional probabilities for each class.
-            tgt (torch.Tensor): Tensor of shape (batch_size, num_atoms) containing the target labels.
-                possible measurement outcomes are [0, 1].
+            tgt (torch.Tensor): Tensor of shape (batch_size, num_atoms, 2) containing the target labels.
+                possible measurement outcomes one-hot encoded [0, 1] or [1, 0].
 
         Returns:
             torch.Tensor: Scalar tensor representing the loss with label smoothing.
         """
         # TODO implement label smoothing
         batchsize = tgt.shape[0]
-        log_probs = torch.einsum("bnd,bnd->b", cond_log_probs, tgt).sum(-1)
+        # log_probs = torch.einsum("bnd,bnd->b", cond_log_probs, tgt).sum(-1)
+        # log_probs = torch.sum(torch.sum(cond_log_probs * tgt, axis=-1), axis=-1)
+        cond_probs = torch.exp(cond_log_probs)  # used for debugging
+        temp = torch.einsum("bnd,bnd->bn", cond_log_probs, tgt)
+        log_probs = torch.sum(temp, axis=-1)
+        # assert torch.allclose(log_probs_test, log_probs_test_2)
+        # assert torch.allclose(log_probs, torch.sum(log_probs_test_2))
         loss = -torch.sum(log_probs) / batchsize
         return loss
