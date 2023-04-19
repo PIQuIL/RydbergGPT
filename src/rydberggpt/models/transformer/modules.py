e6ae6e0b84d0bd6c286d08ab95a03d829d6a07fe
davidfitzek <david.fitzek@protonmail.com>
2023-04-18 22:06:45 -0400

    formatting

--- src/rydberggpt/models/transformer/modules.py
+++ src/rydberggpt/models/transformer/modules.py
@@ -119,24 +119,3 @@ class PositionalEncoding2D(nn.Module):
         pe = torch.cat([pe_1, pe_2], dim=1)
         out = x + pe
         return self.dropout(out)
-
-
-class PositionalEncoding(nn.Module):
-    def __init__(self, d_model, dropout, max_len=5000):
-        super(PositionalEncoding, self).__init__()
-        self.dropout = nn.Dropout(p=dropout)
-
-        # compute the positional encodings once in log space
-        pe = torch.zeros(max_len, d_model)
-        position = torch.arange(0, max_len).unsqueeze(1)
-        div_term = torch.exp(
-            torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model)
-        )
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0)
-        self.register_buffer("pe", pe)
-
-    def forward(self, x):
-        x = x + self.pe[:, : x.size(1)].requires_grad_(False)
-        return self.dropout(x)
--- src/rydberggpt/models/transformer/utils.py
+++ src/rydberggpt/models/transformer/utils.py
@@ -6,20 +6,3 @@ import torch.nn as nn
 def clones(module: nn.Module, n_clones: int):
     """helper function which produces n_clones copies of a layer"""
     return nn.ModuleList([copy.deepcopy(module) for _ in range(n_clones)])
-
-
-# def subsequent_mask(size):
-#     """mask out subsequent positions"""
-#     attn_shape = (size, size)
-#     mask = torch.triu(torch.ones(attn_shape), diagonal=0).type(torch.uint8)
-#     # mask = torch.meshgrid(torch.arange(size), torch.arange(size), indexing="ij")
-#     # mask = mask[0] > mask[1]
-#     # return mask
-#     return mask == 0
-
-
-# def make_std_mask(tgt, pad):
-#     """Create a mask to hide padding and future words."""
-#     tgt_mask = (tgt != pad).unsqueeze(-2)  # noqa
-#     tgt_mask = tgt_mask & subsequent_mask(tgt.size(-1)).type_as(tgt_mask.data)
-#     return tgt_mask
