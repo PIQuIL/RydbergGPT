7fc2af61b1200e8e72281a24700a27b3194e6a3a
davidfitzek <david.fitzek@protonmail.com>
2023-04-26 21:11:09 -0400

    formatting

--- examples/4_pytorch_lightning.py
+++ examples/4_pytorch_lightning.py
@@ -1,16 +1,13 @@
 import argparse
-from dataclasses import asdict, dataclass
 
 import numpy as np
 import pytorch_lightning as pl
 import pytorch_lightning.profilers as pl_profilers
 import torch
-import torch.nn as nn
-import torch.optim as optim
 import torch.profiler
-from config.utils import create_config_from_yaml
 from pytorch_lightning.callbacks import (
     DeviceStatsMonitor,
+    LearningRateMonitor,
     ModelCheckpoint,
     StochasticWeightAveraging,
 )
@@ -20,56 +17,14 @@ from pytorch_lightning.strategies import DDPStrategy
 from rydberggpt.data.loading.rydberg_dataset import get_rydberg_dataloader
 from rydberggpt.models.encoder_decoder_transformer import get_rydberg_encoder_decoder
 from rydberggpt.training.callbacks.module_info_callback import ModelInfoCallback
-from rydberggpt.training.loss import KLLoss
-
-
-class RydbergGPTTrainer(pl.LightningModule):
-    def __init__(
-        self,
-        model,
-        config: dataclass,
-        logger: TensorBoardLogger,
-        example_input_array: torch.tensor = None,
-    ):
-        super().__init__()
-        self.config = config
-        self.save_hyperparameters(asdict(config))
-        self.model = model
-        self.criterion = KLLoss()
-        self.example_input_array = example_input_array
-
-    def forward(self, measurements, cond):
-        out = self.model.forward(measurements, cond)
-        cond_log_probs = self.model.generator(out)
-        return cond_log_probs
-
-    def training_step(self, batch, batch_idx):
-        cond, measurements = batch
-        cond_log_probs = self.forward(measurements, cond)
-        loss = self.criterion(cond_log_probs, measurements)
-        self.log("train_loss", loss, sync_dist=True)
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        cond, measurements = batch
-        cond_log_probs = self.forward(measurements, cond)
-        loss = self.criterion(cond_log_probs, measurements)
-        self.log("val_loss", loss, sync_dist=True)
-        return loss
-
-    # TODO add learning rate scheduler
-    def configure_optimizers(self):
-        optimizer_class = getattr(optim, self.config.optimizer)
-        optimizer = optimizer_class(
-            self.model.parameters(), lr=self.config.learning_rate
-        )
-        return optimizer
+from rydberggpt.training.trainer import RydbergGPTTrainer
+from rydberggpt.utils import create_config_from_yaml
 
 
 def set_example_input_array(train_loader):
     example_batch = next(iter(train_loader))
-    cond, measurements = example_batch
-    return measurements, cond
+    cond, m_shifted_onehot, m_onehot = example_batch
+    return m_onehot, cond
 
 
 def main(config_path: str):
@@ -78,7 +33,7 @@ def main(config_path: str):
     np.random.seed(config.seed)
     device = "cuda" if torch.cuda.is_available() else "cpu"
     config.device = device
-    # TODO make dataloader pytorch_lightning loaders
+
     # https://lightning.ai/docs/pytorch/stable/data/datamodule.html
     train_loader, val_loader = get_rydberg_dataloader(
         config.batch_size, test_size=0.2, num_workers=config.num_workers
@@ -86,8 +41,16 @@ def main(config_path: str):
     input_array = set_example_input_array(train_loader)
 
     # TODO start from checkpoint if specified
-    # TODO add torch compile!
     model = get_rydberg_encoder_decoder(config)
+    if config.compile:
+        # check that device is cuda
+        if device != "cuda":
+            raise ValueError(
+                "Cannot compile model if device is not cuda. "
+                "Please set compile to False."
+            )
+        model = torch.compile(model)
+
     logger = TensorBoardLogger(save_dir="logs")
     log_path = f"logs/lightning_logs/version_{logger.version}"
     rydberg_gpt_trainer = RydbergGPTTrainer(
@@ -104,6 +67,7 @@ def main(config_path: str):
         DeviceStatsMonitor(),
         StochasticWeightAveraging(config.learning_rate),
         ModelInfoCallback(),
+        LearningRateMonitor(logging_interval="step"),
     ]
 
     # https://lightning.ai/docs/pytorch/stable/common/trainer.html
