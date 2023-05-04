fd39602ad2fba33eb27351949931a4b7cbc9c2dc
davidfitzek <david.fitzek@protonmail.com>
2023-05-03 21:28:18 -0400

    update

--- src/rydberggpt/data/dataclasses.py
+++ src/rydberggpt/data/dataclasses.py
@@ -2,17 +2,18 @@ from dataclasses import dataclass
 from typing import List
 
 import torch
+from torch_geometric.data import Batch as PyGBatch
+from torch_geometric.data import Data
 
 
 @dataclass
 class Batch:
-    cond: torch.Tensor
+    graph: Data
     delta: torch.float
     omega: torch.float
     beta: torch.float
     m_onehot: torch.Tensor
     m_shifted_onehot: torch.Tensor
-    coupling_matrix: torch.Tensor
 
 
 def custom_collate(batch: List[Batch]) -> Batch:
@@ -26,15 +27,12 @@ def custom_collate(batch: List[Batch]) -> Batch:
         Batch: A single Batch object containing the collated data.
     """
     batch = Batch(
-        cond=torch.stack([b.cond for b in batch]).to(torch.float32),
+        graph=PyGBatch.from_data_list([b.graph for b in batch]),
         delta=torch.tensor([b.delta for b in batch], dtype=torch.float32),
         omega=torch.tensor([b.omega for b in batch], dtype=torch.float32),
         beta=torch.tensor([b.beta for b in batch], dtype=torch.float32),
         m_onehot=torch.stack([b.m_onehot for b in batch]).to(torch.float32),
         m_shifted_onehot=torch.stack([b.m_shifted_onehot for b in batch]).to(
-            torch.float32
-        ),
-        coupling_matrix=torch.stack([b.coupling_matrix for b in batch]).to(
             torch.float32
         ),
     )
