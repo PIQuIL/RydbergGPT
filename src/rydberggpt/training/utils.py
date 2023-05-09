6308359ac1c88d700b5a3ca64cd383b0494ecdfd
davidfitzek <david.fitzek@protonmail.com>
2023-05-08 23:34:16 -0400

    add docstring

--- src/rydberggpt/training/utils.py
+++ src/rydberggpt/training/utils.py
@@ -1,15 +1,17 @@
-import os
-import re
+from typing import Any, Tuple
+
+from torch.utils.data import DataLoader
 
 
-def get_ckpt_path(from_ckpt: int, log_dir: str = "logs/lightning_logs"):
-    log_dir = os.path.join(log_dir, f"version_{from_ckpt}")
-    # ckpt_path = find_latest_ckpt(from_ckpt, log_dir)
-    if log_dir is None:
-        raise FileNotFoundError(f"No checkpoint found in {log_dir}")
-    return log_dir
+def set_example_input_array(train_loader: DataLoader) -> Tuple[Any, Any]:
+    """
+    Get an example input array from the train loader.
 
+    Args:
+        train_loader (DataLoader): The DataLoader instance for the training data.
 
-def set_example_input_array(train_loader):
+    Returns:
+        Tuple[Any, Any]: A tuple containing m_onehot and graph from the example batch.
+    """
     example_batch = next(iter(train_loader))
     return example_batch.m_onehot, example_batch.graph
