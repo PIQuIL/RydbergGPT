1bc25fd4b85e3863a99650e08a84b85025f956fd
davidfitzek <david.fitzek@protonmail.com>
2023-09-04 18:20:46 +0200

    simplify imports

---
+++ src/rydberggpt/data/loading/__init__.py
@@ -0,0 +1,4 @@
+from .rydberg_dataset import get_rydberg_dataloader
+from .rydberg_dataset_chunked import get_chunked_dataloader
+from .rydberg_dataset_chunked_random import get_chunked_random_dataloader
+from .rydberg_dataset_streaming import get_streaming_dataloader
