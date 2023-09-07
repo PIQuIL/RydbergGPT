47108232c527eb04e09bb6e52b31ec2346801db0
davidfitzek <davidfitzek>
2023-09-07 13:36:15 +0200

    update tensorboard script to look for free port

--- scripts/tensorboard.sh
+++ scripts/tensorboard.sh
@@ -1,3 +1,6 @@
-module purge
+#!/bin/env bash
 
-apptainer exec ~/RydbergGPT/images/pytorch.sif tensorboard --logdir="logs/" --port=6007
+
+module purge
+FREE_PORT=`comm -23 <(seq "8888" "8988" | sort) <(ss -Htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf -n 1`
+apptainer exec ~/RydbergGPT/images/pytorch.sif tensorboard --logdir="logs/" --port=${FREE_PORT}
