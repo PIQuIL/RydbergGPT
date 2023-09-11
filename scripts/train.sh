b9e573c62c6bfe9deb76bfcc68e3eec8cc8d7a32
davidfitzek <davidfitzek>
2023-09-11 14:59:09 +0200

    update train script

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,9 +1,9 @@
 #!/bin/env bash
 
-#SBATCH -A SNIC2022-5-398  # find your project with the "projinfo" command
+#SBATCH -A NAISS2023-5-353  # find your project with the "projinfo" command
 #SBATCH -p alvis
-#SBATCH -t 00-00:15:00
-#SBATCH --gpus-per-node=T4:3
+#SBATCH -t 03-00:00:00
+#SBATCH --gpus-per-node=A100:3
 
 module purge
 
