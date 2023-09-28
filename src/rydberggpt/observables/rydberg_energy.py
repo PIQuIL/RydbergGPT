81e99640bb43f2f26ac3e8c35b0ccb4f1d959747
yhteoh <yhteoh@uwaterloo.ca>
2023-09-28 19:14:43 -0400

    Separated calculation of x magnetization from get_rydberg_enrgy into get_x_magnetization

--- src/rydberggpt/observables/rydberg_energy.py
+++ src/rydberggpt/observables/rydberg_energy.py
@@ -6,7 +6,30 @@ from rydberggpt.models.rydberg_encoder_decoder import RydbergEncoderDecoder
 from rydberggpt.utils import to_one_hot
 
 
-# TODO test function
+@torch.no_grad()
+def get_x_magnetization(
+    model: RydbergEncoderDecoder,
+    samples: torch.Tensor,  # dtype=torch.int64
+    cond: torch.Tensor,  # dtype=torch.float32
+    device: torch.device,
+):
+    # Create all possible states achievable by a single spin flip
+    flipped = (samples[:, None, :] + torch.eye(samples.shape[-1])[None, ...]) % 2
+    flipped = flipped.reshape(-1, samples.shape[-1])
+
+    # Get propabilities of sampled states and the single spin flipped states
+    sample_log_probs = model.get_log_probs(to_one_hot(samples, 2), cond)
+    flipped_log_probs = model.get_log_probs(to_one_hot(flipped, 2), cond)
+    flipped_log_probs = flipped_log_probs.reshape(-1, samples.shape[-1])
+
+    # Calculate ratio of the wavefunction for the sampled and flipped states
+    log_psi_ratio = 0.5 * (flipped_log_probs - sample_log_probs[:, None])
+    psi_ratio = torch.exp(log_psi_ratio)
+
+    x_magnetization = psi_ratio.sum(-1)
+    return x_magnetization
+
+
 @torch.no_grad()
 def get_rydberg_energy(
     model: RydbergEncoderDecoder,
@@ -14,6 +37,7 @@ def get_rydberg_energy(
     cond: torch.Tensor,  # dtype=torch.float32
     device: torch.device,
     undo_sample_path=None,
+    undo_sample_path_args=None,
 ) -> torch.Tensor:
     """
     Calculates energy of the model based on the Hamiltonian defined by cond (graph).
@@ -38,39 +62,41 @@ def get_rydberg_energy(
     beta = cond.x[0, 2]
     Rb = cond.x[0, 3]  # blockade radius
 
-    if undo_sample_path is not None:
-        samples = undo_sample_path(samples)
-
     ########################################################################################
 
-    # Interaction/Rydberg blockade term
+    # Estimate interaction/Rydberg blockade term
+
+    if undo_sample_path is not None:
+        unpathed_samples = undo_sample_path(samples, *undo_sample_path_args)
+    else:
+        unpathed_samples = samples
+
     interaction = (
-        (samples[..., cond.edge_index].prod(dim=-2) * cond.edge_attr[None, ...]).sum(
-            dim=-1
-        )
+        (
+            unpathed_samples[..., cond.edge_index].prod(dim=-2)
+            * cond.edge_attr[None, ...]
+        ).sum(dim=-1)
         * Rb**6
         * omega
     )
 
-    detuning = (delta * samples).sum(1)  # sum over sequence length
+    # Estimate detuning term
+    detuning = (delta * unpathed_samples).sum(1)  # sum over sequence length
 
     # Estimate sigma_x
-    flipped = (samples[:, None, :] + torch.eye(samples.shape[-1])[None, ...]) % 2
-    flipped = flipped.reshape(-1, samples.shape[-1])
-
-    sample_log_probs = model.get_log_probs(to_one_hot(samples, 2), cond)
-    flipped_log_probs = model.get_log_probs(to_one_hot(flipped, 2), cond)
-    flipped_log_probs = flipped_log_probs.reshape(-1, samples.shape[-1])
-
-    log_psi_ratio = 0.5 * (flipped_log_probs - sample_log_probs[:, None])
-
-    psi_ratio = torch.exp(log_psi_ratio)
-
-    offdiag_energy = -(omega * 0.5 * psi_ratio).sum(-1)
+    x_magnetization = get_x_magnetization(model, samples, cond, device)
+    offdiag_energy = -0.5 * omega * x_magnetization
 
     # Diagonal part of energy
     diag_energy = interaction - detuning
 
     energy = diag_energy + offdiag_energy  # Energy estimate
 
-    return torch.stack([energy, interaction, detuning, offdiag_energy]).T
+    return torch.stack(
+        [
+            energy,
+            interaction,
+            detuning,
+            offdiag_energy,
+        ]
+    ).T
