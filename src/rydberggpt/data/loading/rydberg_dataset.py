b91763ac9c8e7eff4f73abde466a6499f8b23ab5
davidfitzek <david.fitzek@protonmail.com>
2023-10-21 21:37:53 +0200

    update dataloader

--- src/rydberggpt/data/loading/rydberg_dataset.py
+++ src/rydberggpt/data/loading/rydberg_dataset.py
@@ -1,106 +1,121 @@
-import json
-import os
+import logging
+import random
 from typing import Tuple
 
-import h5py
 import networkx as nx
 import pandas as pd
 import torch
-from sklearn.model_selection import train_test_split
-from torch.utils.data import DataLoader, Dataset
+from torchdata.dataloader2 import (
+    DataLoader2,
+    DistributedReadingService,
+    InProcessReadingService,
+    MultiProcessingReadingService,
+    PrototypeMultiProcessingReadingService,
+)
+from torchdata.datapipes.iter import FileLister, IterDataPipe
 
 from rydberggpt.data.dataclasses import Batch, custom_collate
-from rydberggpt.data.loading.utils import read_subfolder_data
-from rydberggpt.data.utils_graph import networkx_to_pyg_data
+from rydberggpt.data.utils_graph import pyg_graph_data
 from rydberggpt.utils import to_one_hot
 
+logging.basicConfig(level=logging.INFO)
 
-# NOTE not in use
+
 def get_rydberg_dataloader(
-    batch_size: int = 32,
+    batch_size: int = 10,
     test_size: float = 0.2,
     num_workers: int = 0,
     data_path: str = "dataset",
-) -> Tuple[DataLoader, DataLoader]:
-    df, graph_data = read_subfolder_data(data_path=data_path)
-    # check that df is not empty
-    assert not df.empty, "Dataframe is empty. Check that the data_path is correct."
-    # train_df, test_df = train_test_split(df, test_size=test_size, random_state=42)
-
-    train_dataset = RydbergDataset(df, graph_data)
-    val_dataset = RydbergDataset(df, graph_data)
-
-    train_loader = DataLoader(
-        train_dataset,
-        batch_size=batch_size,
-        shuffle=True,
-        num_workers=num_workers,
-        collate_fn=custom_collate,
+    buffer_size: int = 4,
+) -> Tuple[DataLoader2, DataLoader2]:
+    datapipe = build_datapipes(
+        root_dir=data_path, batch_size=batch_size, buffer_size=buffer_size
     )
-    val_loader = DataLoader(
-        val_dataset,
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=num_workers,
-        collate_fn=custom_collate,
+
+    # rs = DistributedReadingService()
+    # rs = MultiProcessingReadingService(num_workers=num_workers)
+    # rs = PrototypeMultiProcessingReadingService(num_workers=num_workers)
+    rs = InProcessReadingService()
+
+    train_loader = DataLoader2(datapipe=datapipe, reading_service=rs)
+
+    return train_loader, None
+
+
+def select_fn(x):
+    return x[1]
+
+
+def build_datapipes(root_dir: str, batch_size: int, buffer_size: int):
+    file_lister = FileLister([root_dir], recursive=True)
+    config_dp, dataset_dp, graph_dp = file_lister.demux(
+        3, classify_file_fn, drop_none=True, buffer_size=-1
     )
-    return train_loader, val_loader
+    config_dp = config_dp.open_files().parse_json_files().map(select_fn)
+    graph_dp = graph_dp.open_files().parse_json_files().map(select_fn)
+    datapipe = config_dp.zip(dataset_dp).zip(graph_dp).map(map_fn)
+    datapipe = datapipe.shuffle().sharding_filter()
+    datapipe = Buffer(datapipe, buffer_size=buffer_size, batch_size=batch_size).collate(
+        custom_collate
+    )
+
+    return datapipe
 
 
-class RydbergDataset(Dataset):
-    """
-    A custom Dataset class for handling Rydberg atom data.
+def classify_file_fn(filepath):
+    if filepath.endswith("config.json"):
+        return 0
+    if filepath.endswith("dataset.h5"):
+        return 1
+    if filepath.endswith("graph.json"):
+        return 2
+    return None
 
-    Attributes:
-        dataframe (pd.DataFrame): The input dataframe containing Rydberg atom data.
-    """
 
-    def __init__(self, dataframe, graph_data):
-        """
-        Initialize the RydbergDataset with the given dataframe.
+def map_fn(x):
+    return (x[0][0], x[0][1], x[1])
 
-        Args:
-            dataframe (pd.DataFrame): The input dataframe containing Rydberg atom data.
-        """
-        self.dataframe = dataframe
-        self.graph_data = graph_data
 
-    def __len__(self):
-        """
-        Get the number of samples in the dataset.
+class Buffer(IterDataPipe):
+    def __init__(self, source_datapipe, buffer_size, batch_size):
+        self.source_datapipe = source_datapipe
+        self.buffer_size = buffer_size
+        self.batch_size = batch_size
 
-        Returns:
-            int: The length of the dataset.
-        """
-        return len(self.dataframe)
+    def get_sample(self, df, idx):
+        return torch.tensor(df.iloc[idx]["measurement"], dtype=torch.bool)
 
-    def __getitem__(self, index: int) -> Batch:
-        """
-        Get the Batch object at the specified index.
+    def __iter__(self):
+        folder_pairs = list(self.source_datapipe)
+        random.shuffle(folder_pairs)  # Shuffle folder pairs for randomness
 
-        Args:
-            index (int): The index of the Batch object in the dataset.
+        for i in range(0, len(folder_pairs), self.buffer_size):
+            loaded_data = []
+            for j in range(i, min(i + self.buffer_size, len(folder_pairs))):
+                config_file, h5_file_path, graph_file = folder_pairs[j]
+                logging.info(f"Loading: {h5_file_path}")
 
-        Returns:
-            Batch: A Batch object containing the data for the specified index.
-        """
-        row = self.dataframe.iloc[index]
-        graph_dict = self.graph_data[index]  # dict
-        graph_nx = nx.node_link_graph(graph_dict)
+                pyg_graph = pyg_graph_data(config_file, graph_file)
 
-        node_features = torch.tensor(
-            [row["delta"], row["omega"], row["beta"], row["Rb"]],
-            dtype=torch.float32,
-        )
-        pyg_graph = networkx_to_pyg_data(graph_nx, node_features)
+                df = pd.read_hdf(h5_file_path, key="data")
+                for index, row in df.iterrows():
+                    measurement = self.get_sample(df, index)
+                    m_onehot = to_one_hot(measurement, 2)
+                    _, dim = m_onehot.shape
+                    m_shifted_onehot = torch.cat(
+                        (torch.zeros(1, dim), m_onehot[:-1]), dim=0
+                    )
+                    batch = Batch(
+                        graph=pyg_graph,
+                        m_onehot=m_onehot,
+                        m_shifted_onehot=m_shifted_onehot,
+                    )
+                    loaded_data.append(batch)
 
-        measurements = torch.tensor(row["measurement"], dtype=torch.int64)
-        m_onehot = to_one_hot(measurements, 2)  # because Rydberg states are 0 or 1
+            # Shuffle the loaded data for randomness
+            random.shuffle(loaded_data)
 
-        _, dim = m_onehot.shape
-        m_shifted_onehot = torch.cat((torch.zeros(1, dim), m_onehot[:-1]), dim=0)
-        return Batch(
-            graph=pyg_graph,
-            m_onehot=m_onehot,
-            m_shifted_onehot=m_shifted_onehot,
-        )
+            # Yield batches of data
+            for k in range(0, len(loaded_data), self.batch_size):
+                yield loaded_data[k : k + self.batch_size]
+
