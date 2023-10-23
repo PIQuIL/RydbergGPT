37185b33031fd8d6d9baa7fd0099ea618a55b64e
davidfitzek <david.fitzek@protonmail.com>
2023-10-23 17:54:26 +0200

    update test dataloader

--- src/rydberggpt/tests/test_dataloader.py
+++ src/rydberggpt/tests/test_dataloader.py
@@ -2,6 +2,7 @@ import logging
 
 from tqdm import tqdm
 
+from rydberggpt.data.dataclasses import Batch
 from rydberggpt.data.loading.rydberg_dataset import get_rydberg_dataloader
 from rydberggpt.utils import time_and_log
 
@@ -11,6 +12,9 @@ logging.basicConfig(level=logging.INFO)
 
 @time_and_log
 def main():
+    batch_size = 128
+    buffer_size = 2
+    num_workers = 1
     base_dir = "src/rydberggpt/tests/dataset_test/"
 
     dataloader, _ = get_rydberg_dataloader(
@@ -18,13 +22,20 @@ def main():
     )
 
     counter = 0
-
-    # for batch in dataloader:
     for batch in tqdm(dataloader):
-        counter += batch.m_onehot.shape[0]
-        # print(batch.m_onehot.shape)
-
-    print(counter)
+        if counter == 0:
+            assert (
+                batch.m_onehot.shape[0] == batch_size
+            ), f"Batch size of m_onehot is not {batch_size}."
+            assert batch.m_onehot.shape[2] == 2, "Dimension of m_onehot is not 2."
+            assert (
+                batch.m_onehot.shape == batch.m_shifted_onehot.shape
+            ), "Shapes of m_onehot and m_shifted_onehot are not the same."
+            assert isinstance(batch, Batch)
+            assert hasattr(batch, "graph")
+            assert hasattr(batch, "m_onehot")
+            assert hasattr(batch, "m_shifted_onehot")
+        break
 
 
 if __name__ == "__main__":
