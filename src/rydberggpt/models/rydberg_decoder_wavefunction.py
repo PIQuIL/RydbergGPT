Fixed sign of the Rabi term in the Rydberg Hamiltonian for stoquasticity

src/rydberggpt/models/rydberg_decoder_wavefunction.py
@@ -255,13 +255,14 @@ class RydbergDecoderWavefunction(RydbergEncoderDecoder):
         # Diagonal part of energy
         diag_energy = interaction - detuning
 
-        energy = diag_energy + offdiag_energy  # Energy estimate
+        energy = diag_energy - offdiag_energy  # Energy estimate
 
         return torch.stack(
             [
                 energy,
                 interaction,
                 detuning,
+                diag_energy,
                 offdiag_energy,
             ]
         ).T