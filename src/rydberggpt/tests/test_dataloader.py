e881ab245ccb27e41934e3642897a84ab8e8197e
davidfitzek <david.fitzek@protonmail.com>
2024-05-07 14:04:39 +0200

    update tests

--- src/rydberggpt/tests/test_dataloader.py
+++ src/rydberggpt/tests/test_dataloader.py
@@ -1,46 +1,52 @@
 import logging
 
+import torch
+from rydberggpt.data.dataclasses import Batch
+from rydberggpt.data.rydberg_dataset import get_rydberg_dataloader
+from rydberggpt.utils import shift_inputs, time_and_log
 from tqdm import tqdm
 
-from rydberggpt.data.dataclasses import Batch
-from rydberggpt.data.loading.rydberg_dataset import get_rydberg_dataloader
-from rydberggpt.utils import shift_inputs, time_and_log
-
-# setup logging
 logging.basicConfig(level=logging.INFO)
 
 
 @time_and_log
-def main():
+def test_dataloader():
     batch_size = 128
     buffer_size = 2
     num_workers = 1
     base_dir = "src/rydberggpt/tests/dataset_test/"
 
-    dataloader, _ = get_rydberg_dataloader(
-        batch_size=128, data_path=base_dir, buffer_size=2
+    dataloader = get_rydberg_dataloader(
+        batch_size=batch_size,
+        data_path=base_dir,
+        buffer_size=buffer_size,
+        num_workers=num_workers,
     )
 
     counter = 0
     for batch in tqdm(dataloader):
-        if counter == 0:
-            assert (
-                batch.m_onehot.shape[0] == batch_size
-            ), f"Batch size of m_onehot is not {batch_size}."
-            assert batch.m_onehot.shape[2] == 2, "Dimension of m_onehot is not 2."
+        assert (
+            batch.m_onehot.shape[0] == batch_size
+        ), f"Batch size of m_onehot is not {batch_size}."
+        assert batch.m_onehot.shape[2] == 2, "Dimension of m_onehot is not 2."
 
-            m_shifted_onehot = shift_inputs(batch.m_onehot)
-            assert (
-                batch.m_onehot.shape == m_shifted_onehot.shape
-            ), "Shapes of m_onehot and m_shifted_onehot are not the same."
+        m_shifted_onehot = shift_inputs(batch.m_onehot)
+        assert (
+            batch.m_onehot.shape == m_shifted_onehot.shape
+        ), "Shapes of m_onehot and m_shifted_onehot are not the same."
 
-            assert (batch.m_onehot[:, :-1, :] == m_shifted_onehot[:, 1:, :]).all()
-            assert isinstance(batch, Batch)
-            assert hasattr(batch, "graph")
-            assert hasattr(batch, "m_onehot")
-            # assert hasattr(batch, "m_shifted_onehot")
-        # break
+        assert (batch.m_onehot[:, :-1, :] == m_shifted_onehot[:, 1:, :]).all()
+        assert isinstance(batch, Batch)
+        assert hasattr(batch, "graph")
+        assert hasattr(batch, "m_onehot")
+        assert (
+            batch.m_onehot.shape[0] == batch_size
+        ), "Batch size of m_onehot is not 100."
+        assert batch.m_onehot.shape[2] == 2, "Dimension of m_onehot is not 2."
+        assert not torch.isnan(batch.m_onehot).any(), "NaN values found in m_onehot."
+
+        counter += batch.m_onehot.shape[0]
+    print(counter)
 
 
-if __name__ == "__main__":
-    main()
+# test_dataloader()
