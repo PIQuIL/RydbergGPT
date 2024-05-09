5aa6bedc0b20f7f3d1e60e9db2dbf36f038ab44e
davidfitzek <david.fitzek@protonmail.com>
2024-05-09 15:39:59 +0200

    remove cluster scripts

--- scripts/tensorboard.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/env bash
-
-
-module purge
-FREE_PORT=`comm -23 <(seq "8888" "8988" | sort) <(ss -Htan | awk '{print $4}' | cut -d':' -f2 | sort -u) | shuf -n 1`
-apptainer exec ~/RydbergGPT/images/pytorch.sif tensorboard --logdir="logs/" --port=${FREE_PORT}
